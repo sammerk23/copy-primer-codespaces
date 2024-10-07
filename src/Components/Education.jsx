c1f9dc4fc6f60e3b7e487ee9cec1862c748c9644
Kelvin Sammer <sammerx22@gmail.com>
2024-10-07 02:16:46 +0000

    updated

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
 
 const Education = () => {
     return (
-    <section className="light" id="education">
-    <h2>Education</h2>
-    </section>
-);
+        <section className="light" id="education">
+            <h2>Education</h2>
+        </section>
+    );
 };
 
 export default Education;
